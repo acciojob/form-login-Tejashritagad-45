853c3665baa2cd8a55b2bae6c1efd99a9839fa3f
Tejashri Tagad <tagadteju45@gmail.com>
2025-11-26 21:54:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+	
+  event.preventDefault(); // stop form from refreshing the page
+
+  // get values
+  const first = document.querySelector('input[name="fname"]').value.trim();
+  const last = document.querySelector('input[name="lname"]').value.trim();
+
+  // show full name
+  alert(first + " " + last);
 
 }
